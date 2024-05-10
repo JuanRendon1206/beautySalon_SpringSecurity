cfde1e8c3afb4acd14293b995ed1a181b60a55b2
JuanRendon1206 <juanjoseflorezrendon124@gmail.com>
2024-05-10 13:00:23 -0500

    jwt token

--- src/main/java/com/riwi/beautySalon/api/controllers/AuthController.java
+++ src/main/java/com/riwi/beautySalon/api/controllers/AuthController.java
@@ -1,20 +1,35 @@
 package com.riwi.beautySalon.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.riwi.beautySalon.api.dto.request.RegisterReq;
+import com.riwi.beautySalon.api.dto.response.AuthResp;
+import com.riwi.beautySalon.infraestructure.services.AuthServices;
+
+import lombok.AllArgsConstructor;
+
 @RestController
 @RequestMapping
+@AllArgsConstructor
 public class AuthController {
     
+    @Autowired
+    private final AuthServices authService;
+
     @PostMapping(path = "/auth/login")
     public String login(){
         return "FORM LOGIN";
     }
 
     @PostMapping(path = "/auth/register")
-    public String register(){
-        return "DESDE REGISTER";
+    public ResponseEntity<AuthResp> register(@Validated @RequestBody RegisterReq request){
+
+        return ResponseEntity.ok(this.authService.register(request));
     }
 }
